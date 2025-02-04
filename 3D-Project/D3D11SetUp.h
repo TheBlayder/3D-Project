0f9b7b8b813f424f12708ce99655ff6dcc8c18ce
Blayder <hampuse02@gmail.com>
2025-02-04 16:36:52 +0100

    Created D3D11SetUp class

--- 3D-Project/D3D11SetUp.h
+++ 3D-Project/D3D11SetUp.h
@@ -2,6 +2,8 @@
 #include <d3d11.h>
 #include <DirectXMath.h>
 
+#include "Window.h"
+
 class D3D11SetUp
 {
 
@@ -9,15 +11,13 @@ public:
 	D3D11SetUp();
 	~D3D11SetUp();
 
-private:
-	ID3D11Device* device;
-	ID3D11DeviceContext* immediateContext;
-	IDXGISwapChain* swapChain;
-	ID3D11RenderTargetView* rtv;
-	ID3D11Texture2D* depthStencilBuffer;
-	ID3D11DepthStencilView* dsView;
-	D3D11_VIEWPORT viewport;
+	bool SetUp(ID3D11Device*& device, ID3D11DeviceContext*& immediateContext, IDXGISwapChain*& swapChain,
+		ID3D11RenderTargetView*& rtv, ID3D11Texture2D*& dsTexture, ID3D11DepthStencilView*& dsView, D3D11_VIEWPORT& viewport, Window*& window);
 
-public:
+private:
+	bool CreateSwapChain(ID3D11Device*& device, ID3D11DeviceContext*& immediateContext, IDXGISwapChain*& swapChain, Window* window);
+	bool CreateRenderTargetView(ID3D11Device*& device, IDXGISwapChain*& swapChain, ID3D11RenderTargetView*& rtv);
+	bool CreateDepthStencilView(ID3D11Device*& device, Window* window, ID3D11Texture2D*& dsTexture, ID3D11DepthStencilView*& dsView);
+	void SetViewPort(D3D11_VIEWPORT& viewport, Window*& window);
 
 };
