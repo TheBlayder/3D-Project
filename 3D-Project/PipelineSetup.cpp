9faf3eea11d9934ea1ee9ecf0cd0d50fdbd8236a
Hampus Eklund <haek23@student.bth.se>
2025-02-05 15:08:05 +0100

    Set up PipelineSetUp class

--- 3D-Project/PipelineSetup.cpp
+++ 3D-Project/PipelineSetup.cpp
@@ -7,3 +7,36 @@ PipelineSetUp::PipelineSetUp()
 PipelineSetUp::~PipelineSetUp()
 {
 }
+
+bool PipelineSetUp::SetUp(ID3D11Device*& device, ID3D11DeviceContext*& immediateContext, ID3D11Buffer*& vertexBuffer, 
+	ID3D11VertexShader*& vShader, ID3D11PixelShader*& pShader, ID3D11InputLayout*& inputLayout, 
+	ID3D11Buffer*& VScBuffer, ID3D11Buffer*& PScBuffer, 
+	ID3D11Texture2D*& texture, ID3D11ShaderResourceView*& textureView, ID3D11SamplerState*& samplerState, unsigned char*& textureData)
+{
+	return false;
+}
+
+bool PipelineSetUp::LoadShaders(ID3D11Device* device, ID3D11VertexShader*& vShader, ID3D11PixelShader*& pShader, std::string& vShaderByteCode)
+{
+	return false;
+}
+
+bool PipelineSetUp::CreateInputLayout(ID3D11Device* device, ID3D11InputLayout*& inputLayout, const std::string& vShaderByteCode)
+{
+	return false;
+}
+
+bool PipelineSetUp::CreateVertexBuffer(ID3D11Device* device, ID3D11Buffer*& vertexBuffer)
+{
+	return false;
+}
+
+bool PipelineSetUp::CreateTexture(ID3D11Device* device, ID3D11Texture2D*& texture, ID3D11ShaderResourceView*& SRVTexture, unsigned char*& textureData)
+{
+	return false;
+}
+
+bool PipelineSetUp::CreateSamplerState(ID3D11Device* device, ID3D11SamplerState*& samplerState)
+{
+	return false;
+}
