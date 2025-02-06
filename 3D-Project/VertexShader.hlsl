3faad90c5c80b792bb4faf67d7e214dababf8895
Hampus Eklund <haek23@student.bth.se>
2025-02-06 12:48:39 +0100

    compiled a .cso for VS. nothing special

--- 3D-Project/VertexShader.hlsl
+++ 3D-Project/VertexShader.hlsl
@@ -2,20 +2,17 @@
 
 struct VertexShaderInput
 {
-    float3 position;
-    float3 normal;
+    float3 position : POSITION;
 };
 
 struct VertexShaderOutput
 {
     float4 position : SV_POSITION;
-    float3 normal : NORMAL;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
     output.position = float4(input.position, 1.0f);
-    output.normal = input.normal;
     return output;
 }
