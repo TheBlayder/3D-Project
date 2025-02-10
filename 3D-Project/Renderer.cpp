13e4402e6ece7b3faf411771d7c12f6da033c4bf
Blayder <hampuse02@gmail.com>
2025-02-10 18:16:34 +0100

    Made a simple renderer stage

--- 3D-Project/Renderer.cpp
+++ 3D-Project/Renderer.cpp
@@ -8,8 +8,23 @@ Renderer::~Renderer()
 {
 }
 
-void Renderer::RenderFrame(ID3D11DeviceContext* immediateContext, ID3D11RenderTargetView* rtv, ID3D11DepthStencilView* dsView, 
-	D3D11_VIEWPORT& viewport, ID3D11VertexShader* vShader, ID3D11PixelShader* pShader, ID3D11InputLayout* inputLayout, 
-	ID3D11Buffer* vertexBuffer, ID3D11ShaderResourceView* SRVTexture, ID3D11SamplerState* samplerState)
+void Renderer::RenderFrame(ID3D11DeviceContext*& immediateContext, ID3D11RenderTargetView*& rtv, ID3D11DepthStencilView*& dsView, 
+	D3D11_VIEWPORT& viewport, ID3D11VertexShader*& vShader, ID3D11PixelShader*& pShader, ID3D11InputLayout*& inputLayout, 
+	ID3D11Buffer*& vertexBuffer, ID3D11ShaderResourceView*& SRVTexture, ID3D11SamplerState*& samplerState, 
+	const UINT& stride, const UINT& offset, const UINT& nrOfVertices)
 {
+	immediateContext->ClearRenderTargetView(rtv, this->clearColor);
+	immediateContext->ClearDepthStencilView(dsView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1, 0);
+
+	immediateContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+	immediateContext->IASetInputLayout(inputLayout);
+	immediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	immediateContext->RSSetViewports(1, &viewport);
+	immediateContext->VSSetShader(vShader, nullptr, 0);
+	immediateContext->PSSetShader(pShader, nullptr, 0);
+	immediateContext->PSSetShaderResources(0, 1, &SRVTexture);
+	immediateContext->PSSetSamplers(0, 1, &samplerState);
+	immediateContext->OMSetRenderTargets(1, &rtv, dsView);
+
+	immediateContext->Draw(nrOfVertices, 0);
 }
--- 3D-Project/Renderer.h
+++ 3D-Project/Renderer.h
@@ -13,14 +13,18 @@ private:
 
 public:
 	void RenderFrame(
-		ID3D11DeviceContext* immediateContext, 
-		ID3D11RenderTargetView* rtv,
-		ID3D11DepthStencilView* dsView, 
+		ID3D11DeviceContext*& immediateContext, 
+		ID3D11RenderTargetView*& rtv,
+		ID3D11DepthStencilView*& dsView, 
 		D3D11_VIEWPORT& viewport, 
-		ID3D11VertexShader* vShader,
-		ID3D11PixelShader* pShader, 
-		ID3D11InputLayout* inputLayout,
-		ID3D11Buffer* vertexBuffer, 
-		ID3D11ShaderResourceView* SRVTexture, 
-		ID3D11SamplerState* samplerState );
+		ID3D11VertexShader*& vShader,
+		ID3D11PixelShader*& pShader, 
+		ID3D11InputLayout*& inputLayout,
+		ID3D11Buffer*& vertexBuffer, 
+		ID3D11ShaderResourceView*& SRVTexture, 
+		ID3D11SamplerState*& samplerState,
+		const UINT& stride,
+		const UINT& offset,
+		const UINT& nrOfVertices);
+
 };
