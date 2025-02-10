f425514318d10dcbf3c245a996517229fc6cd454
Blayder <hampuse02@gmail.com>
2025-02-10 18:16:07 +0100

    Created boilerplate for the shaders

--- 3D-Project/PixelShader.hlsl
+++ 3D-Project/PixelShader.hlsl
@@ -1,4 +1,12 @@
-float4 main() : SV_TARGET
+struct PSInput
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 position : SV_POSITION;
+    float4 normal : TEXCOORD1;
+    float2 uv : TEXCOORD0;
+};
+
+float4 main(PSInput input) : SV_TARGET
+{
+    // Use input.normal as needed
+    return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
