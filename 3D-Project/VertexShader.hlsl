d1ad2b92ecb29d951ebd79efc011963c1a9cb36b
Hampus Eklund <haek23@student.bth.se>
2025-02-11 12:03:05 +0100

    Revert "compiled a .cso for VS. nothing special"
    This reverts commit 3faad90c5c80b792bb4faf67d7e214dababf8895.

--- 3D-Project/VertexShader.hlsl
+++ 3D-Project/VertexShader.hlsl
@@ -2,17 +2,20 @@
 
 struct VertexShaderInput
 {
-    float3 position : POSITION;
+    float3 position;
+    float3 normal;
 };
 
 struct VertexShaderOutput
 {
     float4 position : SV_POSITION;
+    float3 normal : NORMAL;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
     output.position = float4(input.position, 1.0f);
+    output.normal = input.normal;
     return output;
 }
