5be1587f599f14545061155449841b4b513c1a33
Hampus Eklund <haek23@student.bth.se>
2025-02-14 16:50:09 +0100

    QoL and added some comments

--- 3D-Project/GameObject.cpp
+++ 3D-Project/GameObject.cpp
@@ -1,17 +1,13 @@
+#include <iostream>
+
 #include "GameObject.h"
+#include "ObjectParser.h"
 
 using namespace DirectX;
 
-void GameObject::CreateWorldMatrix(XMMATRIX*& worldMatrix, const XMFLOAT3& scale, const XMFLOAT3& pos, const float& rotationInDeg)
-{
-	XMMATRIX scalingMatrix = XMMatrixScaling(scale.x, scale.y, scale.z);
-	XMMATRIX translationMatrix = XMMatrixTranslation(pos.x, pos.y, pos.z);
-	XMMATRIX rotationnMatrix = XMMatrixRotationY(DX::XMConvertToRadians(rotationInDeg));
-	*worldMatrix = XMMatrixMultiplyTranspose(XMMatrixMultiply(scalingMatrix, translationMatrix), rotationnMatrix);
-}
-
 GameObject::GameObject(std::string* filePath) : staticObject(true), worldPosition(0, 0, 0), worldScale(1, 1, 1), worldRotation(0), worldMatrix(nullptr)
 {
+	// Load object data
 	ObjectParser parser;
 	parser.LoadObjectFromFile(filePath, vertices, normals, UVs);
 }
@@ -21,18 +17,45 @@ GameObject::~GameObject()
 	delete worldMatrix;
 }
 
+// === PRIVATE FUNCTIONS ===
+
+void GameObject::CreateWorldMatrix(XMMATRIX*& worldMatrix, const XMFLOAT3& scale, const XMFLOAT3& pos, const float& rotationInDeg)
+{
+	XMMATRIX scalingMatrix = XMMatrixScaling(scale.x, scale.y, scale.z);
+	XMMATRIX translationMatrix = XMMatrixTranslation(pos.x, pos.y, pos.z);
+	XMMATRIX rotationnMatrix = XMMatrixRotationY(DX::XMConvertToRadians(rotationInDeg));
+	*worldMatrix = XMMatrixMultiplyTranspose(XMMatrixMultiply(scalingMatrix, translationMatrix), rotationnMatrix);
+}
+
+
+// === PUBLIC FUNCTIONS ===
+
 void GameObject::SetWorldData(const DX::XMFLOAT3& scale, const DX::XMFLOAT3& position, const float& rotationInDeg, const bool isStatic)
 {
 	worldPosition = position;
 	worldScale = scale;
 	worldRotation = rotationInDeg;
 	staticObject = isStatic;
-	CreateWorldMatrix(worldMatrix, scale, position, rotationInDeg);
+	CreateWorldMatrix(worldMatrix, worldScale, worldPosition, worldRotation);
 }
 
-bool GameObject::isStatic() const
+bool GameObject::IsStatic() const
 {
 	return staticObject;
+}
+
+void GameObject::UpdateWorldMatrix(DX::XMMATRIX*& worldMatrix, const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rotationInDeg)
+{
+	if (staticObject)
+	{
+		std::cerr << "Warning: Trying to update a static object!" << std::endl;
+		return;
+	}
+
+	worldScale = scale;
+	worldPosition = pos;
+	worldRotation = rotationInDeg;
+	CreateWorldMatrix(worldMatrix, worldScale, worldPosition, worldRotation);
 }
 
 XMMATRIX* GameObject::GetWorldMatrix() const
--- 3D-Project/GameObject.h
+++ 3D-Project/GameObject.h
@@ -1,26 +1,27 @@
 #pragma once
 
-#include <string>
 #include <d3d11.h>
 #include <DirectXMath.h>
 #include <vector>
-
-#include "ObjectParser.h"
+#include <string>
 
 namespace DX = DirectX;
+
 class GameObject
 {
 private:
+	// Object data
 	std::vector<DX::XMFLOAT3> vertices;
 	std::vector<DX::XMFLOAT3> normals;
 	std::vector<DX::XMFLOAT2> UVs;
 
+	// World data
 	DX::XMFLOAT3 worldPosition;
 	DX::XMFLOAT3 worldScale;
 	float worldRotation;
 	DX::XMMATRIX* worldMatrix;
 
-	bool staticObject;
+	bool staticObject; // If the object is static, it will not be updated every frame
 
 	void CreateWorldMatrix(DX::XMMATRIX*& worldMatrix, const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rotationInDeg);
 
@@ -29,7 +30,8 @@ public:
 	~GameObject();
 
 	void SetWorldData(const DX::XMFLOAT3& scale, const DX::XMFLOAT3& position, const float& rotationInDeg, const bool isStatic);
-	bool isStatic() const;
+	bool IsStatic() const;
+	void UpdateWorldMatrix(DX::XMMATRIX*& worldMatrix, const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rotationInDeg);
 
 	DX::XMMATRIX* GetWorldMatrix() const;
 };
