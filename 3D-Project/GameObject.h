9f9ce4fc74eca70b37985a09d5de4a071e360dda
Blayder <hampuse02@gmail.com>
2025-02-28 12:50:20 +0100

    updated naming convention and added getters

--- 3D-Project/GameObject.h
+++ 3D-Project/GameObject.h
@@ -11,17 +11,17 @@ class GameObject
 {
 private:
 	// Object data
-	std::vector<DX::XMFLOAT3> vertices;
-	std::vector<DX::XMFLOAT3> normals;
-	std::vector<DX::XMFLOAT2> UVs;
+	std::vector<DX::XMFLOAT3> m_vertices;
+	std::vector<DX::XMFLOAT3> m_normals;
+	std::vector<DX::XMFLOAT2> m_UVs;
 
 	// World data
-	DX::XMFLOAT3 worldPosition;
-	DX::XMFLOAT3 worldScale;
-	float worldRotation;
-	DX::XMMATRIX* worldMatrix;
+	DX::XMFLOAT3 m_worldPosition;
+	DX::XMFLOAT3 m_worldScale;
+	float m_worldRotation;
+	DX::XMMATRIX* m_worldMatrix;
 
-	bool staticObject; // If the object is static, it will not be updated every frame
+	bool m_staticObject; // If the object is static, it will not be updated every frame
 
 	void CreateWorldMatrix(DX::XMMATRIX*& worldMatrix, const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rotationInDeg);
 
@@ -34,5 +34,7 @@ public:
 	void UpdateWorldMatrix(DX::XMMATRIX*& worldMatrix, const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rotationInDeg);
 
 	const DX::XMMATRIX* GetWorldMatrix() const;
+	const std::vector<DX::XMFLOAT3>& GetVertices() const;
+	const std::vector<DX::XMFLOAT3>& GetNormals() const;
+	const std::vector<DX::XMFLOAT2>& GetUVs() const;
 };
-
