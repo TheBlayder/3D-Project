e963791c5203719431b21042839a98347613f706
Blayder <hampuse02@gmail.com>
2025-03-07 19:28:55 +0100

    Reverted hardcoded vertex data

--- 3D-Project/Cube.cpp
+++ 3D-Project/Cube.cpp
@@ -8,44 +8,5 @@ Cube::Cube(const DX::XMFLOAT3& scale, const DX::XMFLOAT3& pos, const float& rota
 
 void Cube::Init()
 {
-	m_vertices = 
-	{
-		// Front face
-		{ -1.0f, -1.0f, -1.0f },
-		{ -1.0f, 1.0f, -1.0f },
-		{ 1.0f, -1.0f, -1.0f },
-		{ 1.0f, 1.0f, -1.0f },
-		// Back face
-		{ -1.0f, -1.0f, 1.0f },
-		{ 1.0f, -1.0f, 1.0f },
-		{ 1.0f, 1.0f, 1.0f },
-		{ -1.0f, 1.0f, 1.0f }
-	};
 	
-	m_normals = {
-		// Front face
-		{ 0.0f, 0.0f, -1.0f },
-		{ 0.0f, 0.0f, -1.0f },
-		{ 0.0f, 0.0f, -1.0f },
-		{ 0.0f, 0.0f, -1.0f },
-		// Back face
-		{ 0.0f, 0.0f, 1.0f },
-		{ 0.0f, 0.0f, 1.0f },
-		{ 0.0f, 0.0f, 1.0f },
-		{ 0.0f, 0.0f, 1.0f }
-	};
-	
-	m_UVs =
-	{
-		// Front face
-		{ 0.0f, 0.0f },
-		{ 1.0f, 0.0f },
-		{ 1.0f, 1.0f },
-		{ 0.0f, 1.0f },
-		// Back face
-		{ 1.0f, 0.0f },
-		{ 1.0f, 1.0f },
-		{ 0.0f, 1.0f },
-		{ 0.0f, 0.0f }
-	};
 }
