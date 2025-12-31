7581ebaa12b53d05ea0fee4c6518a1bda8c2b572
Blayder <hampuse02@gmail.com>
2025-12-31 13:04:49 +0100

    Deleted strawberry cause it got too annoying, added cake instead :)

--- 3D-Project/TestScene.cpp
+++ 3D-Project/TestScene.cpp
@@ -5,7 +5,7 @@ bool TestScene::LoadScene(ID3D11Device* device, ID3D11DeviceContext* context, Wi
 	using namespace DirectX;
 
 	// Initialize camera
-	XMFLOAT3 camInitialPos = { 0.0f, 0.0f, -3.0f };
+	XMFLOAT3 camInitialPos = { 0.0f, 0.0f, -2.0f };
 	ProjectionData projData;
 	projData.fovInDeg = 90.0f;
 	projData.aspectRatio = static_cast<float>(window.GetWidth()) / static_cast<float>(window.GetHeight());
@@ -17,20 +17,20 @@ bool TestScene::LoadScene(ID3D11Device* device, ID3D11DeviceContext* context, Wi
 	// Initialize light handler
 	m_lightHandler = new LightHandler();
 
-	SpotLightData spotLightData;
-	spotLightData.position = XMFLOAT3(0.f, -2.f, -5.f);
-	spotLightData.intensity = 200.f;
+	SpotLightData spotLightData = {};
+	spotLightData.position = XMFLOAT3(0.f, -1.f, -5.f);
+	spotLightData.intensity = 10.f;
 	spotLightData.color = XMFLOAT4(1.f, 1.f, 1.f, 1.f);
 	spotLightData.direction = XMFLOAT3(0.f, 0.f, 1.f);
-	spotLightData.innerConeInDeg = 15.f;
-	spotLightData.outerConeinDeg = 40.f;
+	spotLightData.innerConeInDeg = 1.f;
+	spotLightData.outerConeinDeg = 30.f;
 	spotLightData.range = 20.f;
 	m_lightHandler->AddSpotLight(spotLightData);
 
-	DirectionalLightData dirLightData;
+	DirectionalLightData dirLightData = {};
 	dirLightData.color = XMFLOAT4(1.f, 1.f, 1.f, 1.f);
 	dirLightData.direction = XMFLOAT3(0.f, -1.f, 0.f);
-	dirLightData.intensity = 100.f;
+	dirLightData.intensity = 1.f;
 	m_lightHandler->AddDirectionalLight(dirLightData);
 	
 	m_lightHandler->Init(device, context, m_camera->GetPosition());
@@ -39,10 +39,10 @@ bool TestScene::LoadScene(ID3D11Device* device, ID3D11DeviceContext* context, Wi
 	
 	// Strawberry
 	Transform strawberryTransform;
-	std::string folderPath = "./Objects/Strawberry";
-	std::string objectName = "Strawberry_obj.obj";
-	std::string textureFolder = "/Texture";
-	strawberryTransform.SetPosition(DirectX::XMVectorSet(0.0f, -5.0f, 5.0f, 0.0f));
+	std::string folderPath = "./Objects/Cake";
+	std::string objectName = "strawberry cake.obj";
+	std::string textureFolder = "/TEXTURES";
+	strawberryTransform.SetPosition(DirectX::XMVectorSet(0.0f, -1.f, 0.0f, 0.0f));
 	strawberryTransform.SetRotation(DirectX::XMVectorSet(-10.0f, 0.f, 0.0f, 0.0f));
 	strawberryTransform.SetScale(DirectX::XMVectorSet(1.f, 1.f, 1.f, 0.0f));
 	AddGameObject(device, strawberryTransform, folderPath, objectName, textureFolder, true);
